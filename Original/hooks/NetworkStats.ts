4fc6c486479e9d8bacd5e6e6b8b56db26f8a17e7
Nisesimadao <121225551+nisesimadao@users.noreply.github.com>
2026-03-08 00:29:10 +0900

    Delete Original directory

--- Original/hooks/NetworkStats.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-
-export interface NetworkStatsPlugin {
-  getTrafficStats(): Promise<{ rxBytes: number; txBytes: number; timestamp: number }>;
-}
-
-const NetworkStats = registerPlugin<NetworkStatsPlugin>('NetworkStats');
-
-export default NetworkStats;
